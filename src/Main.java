fad129927e92f653c855615ce188014261109a56
Абдунаим <a100@MacBook-Air.local>
2022-12-28 21:30:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String word= "Hello java";
+        System.out.println(word.charAt(2));
+        System.out.println(word.substring(2,6));
+        System.out.println(word.contains("ll"));
+        System.out.println(word.toUpperCase());
+        System.out.println(word.toLowerCase());
+    }
+}
